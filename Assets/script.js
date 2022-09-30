8e5c208390a273da77d6cf9b4f8ab72f7462d53f
William James <Williamj3795@gmail.com>
2022-09-29 20:01:19 -0400

    making media query responsive

--- Assets/script.js
+++ Assets/script.js
@@ -109,113 +109,56 @@ console.log(lat);
   function kToF(K) {
         return Math.floor((K - 273.15) * 1.8 + 32);
     }
- // construct the url with parameter values
- 
- var apikey = "8q66bc7bcvegg9xr7ed7xg6u";
 
- var baseUrl = "http://data.tmsapi.com/v1.1";
 
- var showtimesUrl = baseUrl + '/movies/showings';
+// movie API information.
+const apiUrl = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=974257a59529a643b1516d5cebdd63c2';
+const IMGPATH = "https://image.tmdb.org/t/p/w1280";
+const SEARCHAPI =
+    "https://api.themoviedb.org/3/search/movie?&api_key=974257a59529a643b1516d5cebdd63c2&query=";
 
- var zipCode = `${lat};${lon}`;
+function showMovies(apiUrl){
+    fetch(apiUrl).then(res => res.json())
+    .then(function(data){
+console.log(data);
+    data.results.forEach(element => {
+      // Creating elemnts for our data inside the container  
+     
+        const el = document.createElement('div');
+        el.id = "movie-item";
+        const image = document.createElement('img');
+        const text = document.createElement('h2');
 
- var d = new Date();
+        text.innerHTML = `${element.title}`;
+        image.src = IMGPATH + element.poster_path;
+        el.appendChild(image);
+        el.appendChild(text);
+    
+        container.appendChild(el);
+    }); 
+});
+}
 
- var today = d.getFullYear() + '-' + (d.getMonth()+1) + '-' + d.getDate();
+//submit search input
+movieForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+     
+    const searchTerm = search.value.trim();
 
+    if (searchTerm) {
+        showMovies(SEARCHAPI + searchTerm);
+        search.value = "";
+    }
+});
+
+//scroll down page function
+ const element = document.getElementsByTagName("html")
+const scrollToBottom = (element) => {
+	
+   element.scrollTop = element.scrollHeight;
+}
+
+const scrollToTop = (element) => {
   
-
- $(document).ready(function() {
-
-  
-
-   // send off the query
-
-   $.ajax({
-
-    url: showtimesUrl,
-
-        data: { startDate: today,
-
-            zip: zipCode,
-
-            jsonp: "dataHandler",
-
-            api_key: apikey
-
-           },          
-
-    dataType: "jsonp",
-
-   });
-
- });
-
-  
-
- // callback to handle the results
-
- function dataHandler(data) {
-
-  $(document.body).append('<p>Found ' + data.length + ' movies showing within 5 miles of ' + zipCode+':</p>');
-
-  var movies = data.hits;
-
-  $.each(data, function(index, movie) {
-
-    var movieData = '<div class="tile"><img src="http://developer.tmsimg.com/' + movie.preferredImage.uri + '?api_key='+apikey+'"><br/>';
-
-    movieData += movie.title;
-
-    if (movie.ratings) { movieData += ' (' + movie.ratings[0].code + ') </div>' };
-
-    $(document.body).append(movieData);
-  });
- };
-
-
-
-
-//  function movieAddress(lat,lon){
-//   var myHeaders = new Headers();
-//   myHeaders.append("client", "CASE");
-//   myHeaders.append("x-api-key", "j25XOEFpKu6Tfx1iQWGZb4NqNhB88bwa8mJYCrYy");
-//   myHeaders.append("authorization", "Basic Q0FTRTpKblNFeWVodFIzazA=");
-//   myHeaders.append("territory", "US");
-//   myHeaders.append("api-version", "v200");
-//   myHeaders.append("geolocation", `${lat};${lon}`);
-//   myHeaders.append("device-datetime", "2022-09-25T14:06:21.449Z");
-
-//   var requestOptions = {
-//     method: 'GET',
-//     headers: myHeaders,
-//     redirect: 'follow'
-//   };
-
-//   fetch("https://api-gate2.movieglu.com/cinemasNearby/?n=5", requestOptions)
-//     .then(response => response.text())
-//     .then(function(data){
-//       console.log(JSON.stringify(data));
-//       //display the data
-      
-//       for(let i=0; i<5;i++){
-
-//       var cinemaInfo = {
-//       cinemaName: data.cinemas[i].cinema_name,
-//       cinemaAddress: data.cinemas[i].address
-//   };
-      
-//       var cinemas = document.createElement("ul");
-//       var cinema = document.createElement("li");
-//       cinema[0].setAttribute("style","font-size:20px;color:blue;");
-//       cinema.innerHTML = cinemaInfo;
-  
-//       cinemas.appendChild(cinema);
-//       container.appendChild(cinemas);
-      
-// }
-// })
-//   .catch(error => console.log('error', error));
-// }
-
-// movieAddress(41.4995,-81.6954);
+   element.scrollTop = 0;
+}
